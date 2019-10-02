d3effbea64326ccfcc7a821cf94f5c0c499471d7
Eli Kim <eliskim27@gmail.com>
2019-10-02 23:59:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,7 @@ def monopoly_with_third_tier
 	    :names => {
 	      :reading_railroad => {},
 	      :pennsylvania_railroad => {},
-	      :short_railroad => {},
+	      :b_and_o_railroad => {},
 	      :readin_railroad => {}
 	    }
 	  }
