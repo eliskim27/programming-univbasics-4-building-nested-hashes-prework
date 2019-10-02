42a340fae0080c838c25cf3889e60f6f7681a41d
Eli Kim <eliskim27@gmail.com>
2019-10-02 23:53:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,7 @@ def monopoly_with_third_tier
 	  :railroads => {
 	    :pieces => 4,
 	    :rent_in_dollars => {
-	      :a => "a",
+	      :names => "a",
 	      :b => "b",
 	      :c => "c",
 	      :e => "e"
